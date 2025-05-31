e4702b55bf86f28bdc38e5571e2dc7f0e86a9e1d
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-31 15:25:22 +0200

    Update install.sh

--- project/install.sh
+++ project/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo ln -sf "/usr/bitey/software/biteydb/bin/biteydb" "/usr/bin/biteydb",
+sudo ln -sf "/usr/bitey/software/biteydb/bin/biteydb" "/usr/bin/biteydb"
 sudo ln -sf "/usr/bitey/software/biteydb/bin/biteytags" "/usr/bin/biteytags"
